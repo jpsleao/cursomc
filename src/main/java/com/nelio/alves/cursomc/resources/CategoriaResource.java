0909bf6134e3d55fe9ad77565f9f1e9039b5d8b7
João Paulo <jpsleao@uol.com.br>
2018-04-05 18:44:23 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/nelio/alves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelio/alves/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,5 @@
 package com.nelio.alves.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +20,7 @@ public class CategoriaResource {
 	@RequestMapping(value="/{id}",method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = srv.buscar(id);
-
+		
 		return ResponseEntity.ok().body(obj);
 	}
 
--- src/main/java/com/nelio/alves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelio/alves/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.nelio.alves.cursomc.dao.CategoriaDao;
 import com.nelio.alves.cursomc.domains.Categoria;
+import com.nelio.alves.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = dao.findById(id);
-		return obj.orElse(null);	   	
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));	   	
 	}
 
 }
