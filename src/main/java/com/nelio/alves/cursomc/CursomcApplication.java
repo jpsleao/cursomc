4d7982cb8d460f85a56ee6b0ccbd2803584845ff
João Paulo <jpsleao@uol.com.br>
2018-04-05 17:25:14 -0300

    Criando operação de instaciação

--- src/main/java/com/nelio/alves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelio/alves/cursomc/CursomcApplication.java
@@ -1,12 +1,30 @@
 package com.nelio.alves.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.nelio.alves.cursomc.dao.CategoriaDao;
+import com.nelio.alves.cursomc.domains.Categoria;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaDao categoriaDao;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaDao.saveAll(Arrays.asList(cat1,cat2));
 	}
 }
