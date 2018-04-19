ae5e250dcb5516a732bc2fa4c3350319b1defd69
João Paulo <jpsleao@uol.com.br>
2018-04-19 12:23:33 -0300

    Listando todas categorias com DTO

--- src/main/java/com/nelio/alves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelio/alves/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.nelio.alves.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nelio.alves.cursomc.domains.Categoria;
+import com.nelio.alves.cursomc.dto.CategoriaDTO;
 import com.nelio.alves.cursomc.services.CategoriaService;
 
 @RestController
@@ -28,6 +31,13 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = srv.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
 		obj = srv.insert(obj);
